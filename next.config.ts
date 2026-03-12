783ea13d2147a9f694b67b8cd9c5d176ddfa3bab
Yuri Valença <chechaboy0012@gmail.com>
2026-03-12 14:08:37 -0300

    system: :wrench: Update project config to accomodate Linara

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+import withLinaria from 'next-with-linaria';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withLinaria(nextConfig);
