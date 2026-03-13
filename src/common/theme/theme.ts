eb3cfa0fa43df68069589b93dcf96e10ab70b4b0
YuriValenca <valencacunhayuri@gmail.com>
2026-03-13 07:46:43 -0300

    feat :art: Implement theme file on project

---
+++ src/common/theme/theme.ts
@@ -0,0 +1,32 @@
+export const theme = {
+  colors: {
+    // Add colors throughout the project
+  },
+  spacing: {
+    none: "0",
+    nano: "2px",
+    micro: "4px",
+    xsmall: "8px",
+    small: "12px",
+    default: "16px",
+    medium: "24px",
+    large: "32px",
+    xlarge: "36px",
+    xxlarge: "40px",
+    xxxlarge: "48px",
+    huge: "56px",
+    enormous: "64px",
+    100: "100px",
+    HUEG: "256px",
+  },
+  fontSize: {
+    tooltip: "10px",
+    sm: "12px",
+    normal: "16px",
+    h5: "20px",
+    h4: "24px",
+    h3: "28px",
+    h2: "32px",
+    h1: "36px",
+  }
+} as const;
