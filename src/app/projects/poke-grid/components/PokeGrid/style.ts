0b85a6f2c213697f17bec64a046e10f554964316
YuriValenca <valencacunhayuri@gmail.com>
2026-03-25 17:02:38 -0300

    fix: :sparkles: Add autocomplete input and pokemon selection

--- src/app/projects/poke-grid/components/PokeGrid/style.ts
+++ src/app/projects/poke-grid/components/PokeGrid/style.ts
@@ -3,10 +3,9 @@ import styled from "@emotion/styled";
 
 export const PokeGridContainer = styled.div`
   background-color: #101833;
-  height: calc(100dvh - ${theme.spacing.xxlarge});
+  height: 100dvh;
   width: 100dvw;
-  margin-top: ${theme.spacing.xxlarge};
-  padding-bottom: ${theme.spacing.xxxlarge};
+  padding-bottom: ${theme.spacing[100]};
 
   display: flex;
   flex-direction: column;
@@ -16,14 +15,36 @@ export const PokeGridContainer = styled.div`
 
 export const Title = styled.h3`
   font-size: ${theme.fontSize.h3};
-  margin-bottom: ${theme.spacing.default};
 `;
 
 export const PokeGridContent = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 120px);
-  grid-template-rows: repeat(4, 120px);
-  gap: ${theme.spacing.nano};
   justify-items: center;
+  align-items: center;
+  grid-template-columns: repeat(5, 128px);
+  grid-template-rows: repeat(4, 128px);
+  gap: ${theme.spacing.nano};
+  margin-bottom: ${theme.spacing.medium};
+
+  > *:nth-child(7) {
+    border-top-left-radius: ${theme.borderRadius.medium};
+  }
+
+  > *:nth-child(9) {
+    border-top-right-radius: ${theme.borderRadius.medium};
+  }
+
+  > *:nth-child(17) {
+    border-bottom-left-radius: ${theme.borderRadius.medium};
+  }
+
+  > *:nth-child(19) {
+    border-bottom-right-radius: ${theme.borderRadius.medium};
+  }
+`;
+
+export const ScoreWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   align-items: center;
 `;
