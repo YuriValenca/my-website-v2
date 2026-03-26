2bd52fa316bb1b64d84f33ea92f203a7485fb644
YuriValenca <valencacunhayuri@gmail.com>
2026-03-26 09:17:40 -0300

    system: :art: Apply new font to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { QueryProvider } from './components/queryProvider'
+import { QueryProvider } from '../shared/components/queryProvider'
+import { notoSansFont } from '@/shared/theme/theme'
 
 export const metadata: Metadata = {
   title: 'Yuri Valença - Portfolio',
@@ -13,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={notoSansFont.variable}>
       <body className="bg-gray-900 text-white antialiased">
         <QueryProvider>
           {children}
