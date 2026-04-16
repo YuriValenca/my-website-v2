a120285148cf6e39136aaf3f35ffd76e714ccc28
YuriValenca <valencacunhayuri@gmail.com>
2026-04-16 14:11:41 -0300

    fix: :arrow_up: Update Next version due to version vulnerability

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,13 @@ const nextConfig: NextConfig = {
   compiler: {
     emotion: true,
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
 };
 
