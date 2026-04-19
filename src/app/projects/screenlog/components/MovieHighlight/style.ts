137f423c80d913f0ebbe88d55aef7c9b7761cf07
YuriValenca <valencacunhayuri@gmail.com>
2026-04-19 19:28:01 -0300

    feat: :sparkles: Implement and display Movie Highlight component

---
+++ src/app/projects/screenlog/components/MovieHighlight/style.ts
@@ -0,0 +1,101 @@
+import styled from "@emotion/styled";
+import { theme } from "@/shared/theme/theme";
+
+const getRatingColor = (vote: number | undefined): string => {
+  if (!vote) return theme.colors.feedback.badRating;
+  if (vote >= 7) return theme.colors.feedback.goodRating;
+  if (vote >= 5) return theme.colors.feedback.avgRating;
+  if (vote >= 3) return theme.colors.feedback.badRating;
+  return theme.colors.feedback.horribleRating;
+};
+
+export const MovieHighlightWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: ${theme.spacing.large};
+
+  img {
+    border: 2px solid ${theme.colors.border.default};
+    border-radius: ${theme.borderRadius.medium};
+    width: 200px;
+    height: 300px;
+    cursor: pointer;
+    transition: transform 0.3s ease;
+
+    :hover {
+      transform: scale(1.05);
+    }
+  }
+`;
+
+export const DetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.spacing.small};
+  color: ${theme.colors.text.secondary};
+  font-size: ${theme.fontSize.md};
+`;
+
+export const Tags = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+export const Tag = styled.button`
+  font-family: ${theme.fontFamily.noto};
+  font-size: ${theme.fontSize.tooltip};
+  text-transform: uppercase;
+  background-color: transparent;
+  padding: ${theme.spacing.micro};
+  border: 1px solid ${theme.colors.border.default};
+  color: ${theme.colors.text.placeholder};
+  border-radius: ${theme.borderRadius.small};
+  line-height: 1.25;
+  letter-spacing: 2px;
+  cursor: pointer;
+  transition: opacity 0.25s ease-in-out;
+
+  :hover {
+    opacity: 0.7;
+  }
+`;
+
+export const Title = styled.h3``;
+
+export const ExtraInfo = styled.div``;
+
+export const RatingWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${theme.spacing.xsmall};
+`;
+
+export const Rating = styled.span<{ voteValue: number | undefined }>`
+  color: ${({ voteValue }) => getRatingColor(voteValue)};
+  font-size: ${theme.fontSize.h4};
+  font-weight: bold;
+`;
+
+export const Overview = styled.p`
+
+`;
+
+export const Cast = styled.div``;
+
+export const CastAvatar = styled.div``;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: ${theme.spacing.small};
+
+  button {
+    font-size: ${theme.fontSize.md};
+    padding: ${theme.spacing.xsmall} ${theme.spacing.medium};
+  }
+
+  button:last-of-type {
+    border: 1px solid ${theme.colors.border.default};
+  }
+`;
--- src/app/projects/screenlog/components/Screenlog/index.tsx
+++ src/app/projects/screenlog/components/Screenlog/index.tsx
@@ -2,15 +2,26 @@
 
 import MovieSection from "../MovieSection";
 import { ScreenlogWrapper } from "./style";
-
+import { useGetGenres, useGetLandingPageData } from "../../hooks/useMovies";
+import MovieHighlight from "../MovieHighlight";
+import GenreContext from "../../context/context";
 
 const Screenlog = () => {
+  const { latest, popular, topRated, isLoading, isError } = useGetLandingPageData({ enabled: true });
+  const { data: genreList = [] } = useGetGenres();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>
+
   return (
-    <ScreenlogWrapper>      
-      <MovieSection title="Latest" hook="latest" />
-      <MovieSection title="Popular" hook="popular" />
-      <MovieSection title="Top Rated" hook="top-rated" />
-    </ScreenlogWrapper>
+    <GenreContext.Provider value={genreList}>
+      <ScreenlogWrapper>
+        <MovieHighlight data={popular?.results[0]} totalResults={popular?.total_results} />
+        <MovieSection title="Latest" data={latest} />
+        <MovieSection title="Popular" data={popular} />
+        <MovieSection title="Top Rated" data={topRated} />
+      </ScreenlogWrapper>
+    </GenreContext.Provider>
   )
 }
 
