71b5f427e54ff111f7d8a7b795465aa536b01a33
YuriValenca <valencacunhayuri@gmail.com>
2026-04-19 19:24:56 -0300

    fix: :nail_care: Adjust layout and outer components style

--- src/app/projects/screenlog/components/Screenlog/style.ts
+++ src/app/projects/screenlog/components/Screenlog/style.ts
@@ -3,4 +3,8 @@ import { theme } from "@/shared/theme/theme";
 
 export const ScreenlogWrapper = styled.div`
   height: calc(100dvh - (${theme.spacing.xxxlarge} + ${theme.spacing.huge}));
+  padding: ${theme.spacing.medium} 10% ${theme.spacing.none};
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.spacing.medium};
 `;
