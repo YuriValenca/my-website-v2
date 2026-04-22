f58732f52ce582340dcb9295a370feda2f69f0fc
YuriValenca <valencacunhayuri@gmail.com>
2026-04-22 16:03:59 -0300

    feat: :sparkles: Create MovieSection component

---
+++ src/app/projects/screenlog/components/MovieSection/index.tsx
@@ -0,0 +1,21 @@
+import { MovieList } from "../../types/movies";
+
+
+interface MovieSectionProps {
+  title: string;
+  data: MovieList | undefined;
+}
+
+const MovieSection = ({
+  title,
+  data
+}: MovieSectionProps) => {
+
+  return (
+    <div>
+      {title}
+    </div>
+  )
+}
+
+export default MovieSection;
